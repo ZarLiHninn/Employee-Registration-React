4fb9e0da022ebcffbb1f2ae6df89e9aec5d258f2
Zar Li Hnin <sakurazarli@gmail.com>
2020-08-05 19:00:35 +0630

    first commit

---
+++ src/main/java/com/example/demo/config/Configuration.java
@@ -0,0 +1,18 @@
+package com.example.demo.config;
+
+import java.util.Arrays;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@org.springframework.context.annotation.Configuration
+public class Configuration extends WebMvcConfigurerAdapter {
+
+	@Bean
+	@Qualifier("genders")
+	public List<String> genders() {
+		return Arrays.asList("MALE", "FEMALE");
+	}
+
+}
